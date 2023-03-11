842816d9eeaa553fff5a86e5c3675c6d42b6ffdd
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-03-11 10:58:22 +0300

    Added task_68

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        static int GetNumberUserMassedge(string userMassedge)
+        {
+            int userNumber = 0;
+            while (true)
+            {
+                Console.Write(userMassedge);
+                bool check = int.TryParse(Console.ReadLine(), out userNumber);
+                if (check) return userNumber;
+                else Console.WriteLine("Ошибка! Попробуйте еще раз.");
+            }
+        }
+
+        static int GetFunctionAkkerman(int numberM, int numberN)
+        {
+            if (numberM == 0) return numberN + 1;
+            if (numberM > 0 && numberN == 0) return GetFunctionAkkerman(numberM - 1, 1);
+            if (numberM > 0 && numberN > 0) return GetFunctionAkkerman(numberM - 1, GetFunctionAkkerman(numberM, numberN -1));
+            return GetFunctionAkkerman(numberM, numberN);
+        }
+        //        Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+        //        Даны два неотрицательных числа m и n.
+        //        m = 2, n = 3->A(m, n) = 9
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberM = GetNumberUserMassedge("Введите число М: ");
+            int numberN = GetNumberUserMassedge("Введите число N: ");
+            int functionAkkerman = GetFunctionAkkerman(numberM, numberN);
+            Console.WriteLine(functionAkkerman);
         }
     }
 }

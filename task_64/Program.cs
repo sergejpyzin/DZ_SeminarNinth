f9f49ddfb53a32fafd7951cca9aff0e0ae4b2e93
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-03-11 09:48:43 +0300

    Добавьте файлы проекта.

---
+++ task_64/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace task_64
+{
+    internal class Program
+    {
+        static int GetNumberUserMassedge(string userMassedge)
+        {
+            int userNumber = 0;
+            while (true)
+            {
+                Console.Write(userMassedge);
+                bool check = int.TryParse(Console.ReadLine(), out userNumber);
+                if (check) return userNumber;
+                else Console.WriteLine("Ошибка! Попробуйте еще раз.");
+            }
+        }
+
+        static void NaturalNumber(int number)
+        {
+            if (number <= 0) return;
+            Console.WriteLine(number);
+            NaturalNumber(number - 1);
+        }
+//          Задача 64: Задайте значение N.
+//          Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//          N = 5 -> "5, 4, 3, 2, 1"
+//          N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+        static void Main(string[] args)
+        {
+            int number = GetNumberUserMassedge("Введите число: ");
+            NaturalNumber(number);
+        }
+    }
+}
